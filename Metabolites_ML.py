7e0e6ff7a4967ebc13f2e62a2646a51c31c597db
AbdouMousas <abazhag@hotmail.com>
2022-01-20 20:28:33 +0200

    Clustering

--- Metabolites_ML.py
+++ Metabolites_ML.py
@@ -35,3 +35,61 @@ rf2.score(Xthin,y)
 rf2.feature_importances_
 
 plt.bar(range(len(rf2.feature_importances_)),rf2.feature_importances_)
+
+########################################################################################
+# Βlock modeling
+########################################################################################
+names = X.columns
+
+idx = numpy.zeros(len(names))
+corr= X.corr(method="spearman").abs().values
+threshold = 0.8
+ii = 0
+exit = 0
+
+while exit != 1:
+    index = numpy.where(corr[0,:] > threshold)[0]
+    corr = numpy.delete(corr,0,axis=0)
+    corr = numpy.delete(corr,0,axis=1)
+    if len(index) > 1:
+       idx[index] = ii + 1
+       ii = ii + 1
+    if corr.shape[0] == 0:
+        exit = 1
+    
+
+corr= X.corr(method="spearman").abs().values
+threshold = 0.8    
+clusters = []
+seeds = numpy.arange(len(corr))
+
+while len(seeds) > 0:
+    index = numpy.where(corr[seeds[0],:] > threshold)[0]
+    clusters.append(index)
+    seeds = numpy.setdiff1d(seeds,index)
+
+
+suma = 0
+for i in range(len(clusters)):
+    suma = suma + len(clusters[i])
+
+numpy.argsort(idx)
+
+ind = []
+for i in range(len(corr)):
+   index = numpy.where((corr[i,:] > threshold) & (corr[i,:] < 1))[0]
+   ind.extend(index)
+
+ind = sorted(list(set(ind)))
+
+
+ind = []
+for i in range(len(corr)):
+   index = numpy.where(corr[i,:] > threshold)[0]
+   ind.append(index)
+
+index1 = []
+for i in range(len(ind)):
+   if 133 in ind[i]:
+       index1.append(i)
+
