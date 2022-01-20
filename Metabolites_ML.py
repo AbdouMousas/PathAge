2b25dfe6c4c5277c15a4dfb4f8c26d9ea5a995f9
AbdouMousas <abazhag@hotmail.com>
2022-01-20 15:42:07 +0200

    Feature Importances

--- Metabolites_ML.py
+++ Metabolites_ML.py
@@ -1,6 +1,18 @@
-import pandas
+import pandas, numpy, seaborn
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
 
 filePath = "/home/amousas/Downloads/metabol.xlsx"
 
 data = pandas.read_excel(filePath)
+dataNaN = data.dropna()
 
+X = dataNaN.iloc[:,range(139)]
+y = dataNaN.iloc[:,140]
+
+rf = RandomForestClassifier(random_state=1983)
+rf.fit(X, y)
+rf.score(X,y)
+rf.feature_importances_
+
+plt.bar(range(139),rf.feature_importances_)
