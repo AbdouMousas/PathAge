aaf76f1b3d3675d4534cda94bf91f1da909d6c3e
AbdouMousas <abazhag@hotmail.com>
2022-01-21 12:30:29 +0200

    Plotly

--- Metabolites_ML.py
+++ Metabolites_ML.py
@@ -93,3 +93,64 @@ for i in range(len(ind)):
    if 133 in ind[i]:
        index1.append(i)
 
+seaborn.heatmap(corr)
+
+import scipy
+import scipy.cluster.hierarchy as sch
+
+def cluster_corr(corr_array, inplace=False):
+    """
+    Rearranges the correlation matrix, corr_array, so that groups of highly 
+    correlated variables are next to eachother 
+    
+    Parameters
+    ----------
+    corr_array : pandas.DataFrame or numpy.ndarray
+        a NxN correlation matrix 
+        
+    Returns
+    -------
+    pandas.DataFrame or numpy.ndarray
+        a NxN correlation matrix with the columns and rows rearranged
+    """
+    pairwise_distances = sch.distance.pdist(corr_array)
+    linkage = sch.linkage(pairwise_distances, method='complete')
+    cluster_distance_threshold = pairwise_distances.max()/2
+    idx_to_cluster_array = sch.fcluster(linkage, cluster_distance_threshold, 
+                                        criterion='distance')
+    idx = np.argsort(idx_to_cluster_array)
+    
+    if not inplace:
+        corr_array = corr_array.copy()
+    
+    if isinstance(corr_array, pd.DataFrame):
+        return corr_array.iloc[idx, :].T.iloc[idx, :]
+    return corr_array[idx, :][:, idx]
+
+
+corrReArrange = cluster_corr(corr)
+corrReArrangePD = cluster_corr(X.transpose())
+
+seaborn.heatmap(cluster_corr(corr))
+
+
+
+pairwise_distances = sch.distance.pdist(corr)
+linkage = sch.linkage(pairwise_distances, method='complete')
+cluster_distance_threshold = pairwise_distances.max()/2
+idx_to_cluster_array = sch.fcluster(linkage, cluster_distance_threshold, 
+                                        criterion='distance')
+idx = numpy.argsort(idx_to_cluster_array)
+
+import plotly.express as px
+fig = px.bar(y=rf.feature_importances_,x=X.columns.values)
+#fig.write_image("/home/amousas/Downloads/test.png")
+fig.write_html("/home/amousas/Downloads/test.html")
+
+fig2 = px.imshow(X.corr(method="spearman").abs())
+fig2.write_html("/home/amousas/Downloads/test2.html")
+
+fig3 = px.imshow(corrReArrange)
+fig3.write_html("/home/amousas/Downloads/test3.html")
+
+
